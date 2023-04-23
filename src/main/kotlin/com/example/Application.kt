d6e0d95c3351f7793ec27bf2ba49111ce2664a17
marcos.giovanni <marcos.giovanni@superside.com>
2023-04-23 20:59:58 +0100

    cleanup

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,10 @@
 package com.example
 
+import com.example.plugins.configureRouting
+import com.example.plugins.configureStatus
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
-import io.ktor.server.plugins.statuspages.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
--- src/main/kotlin/com/example/exception/MissingParameterException.kt
+++ src/main/kotlin/com/example/exception/MissingParameterException.kt
@@ -1,7 +1,5 @@
 package com.example.exception
 
-import java.lang.reflect.Parameter
-
 class MissingParameterException(
     val parameterName: String
 ): Throwable()
